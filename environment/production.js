3e84e49e14f79f418f2fbc896a23b077c21c8af5
unknown <Urubaean>
2020-08-31 14:37:03 +0200

    fix

--- environment/production.js
+++ environment/production.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
     dbUrl: 'mongodb+srv://lokki:aze@cluster0.wpajw.mongodb.net/twitter?retryWrites=true&w=majority',
-    cert: ('/etc/letsencrypt/live/www.lokki-testproject.site/fullchain.pem'),
-    key: ('/etc/letsencrypt/live/www.lokki-testproject.site/privkey.pem'),
+    cert: '/etc/letsencrypt/live/www.lokki-testproject.site/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.lokki-testproject.site/privkey.pem',
     portHttp:80,
     portHttps:443
 };
